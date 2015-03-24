dd2dac8a55eac20621635060be3ec0dbeaea9655
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2015-03-24 11:54:05 +0200

    Fix import error for manila tab
    After sync with latest Horizon one of import became incorrect.

--- openstack_dashboard/dashboards/admin/shares/utils.py
+++ openstack_dashboard/dashboards/admin/shares/utils.py
@@ -14,7 +14,7 @@
 
 from horizon import exceptions
 from openstack_dashboard.api import keystone
-from openstack_dashboard.openstack.common.gettextutils import _  # noqa
+from openstack_dashboard.openstack.common._i18n import _  # noqa
 
 
 def set_tenant_name_to_objects(request, objects):

71e2f1e3105760a211f0a9f438e6137692eabbd3
Ponomaryov Valeriy <vponomaryov@mirantis.com>
2015-04-16 14:17:05 +0300

    Add nova-network support to share-networks in admin panel
    For the moment only project/shares tab has support of the Nova networking.
    Add it to admin/shares tab too.

--- openstack_dashboard/dashboards/admin/shares/tabs.py
+++ openstack_dashboard/dashboards/admin/shares/tabs.py
@@ -18,7 +18,9 @@ from django.utils.translation import ugettext_lazy as _
 from horizon import exceptions
 from horizon import tabs
 
+from openstack_dashboard.api import base
 from openstack_dashboard.api import manila
+from openstack_dashboard.api import network
 from openstack_dashboard.api import neutron
 
 from openstack_dashboard.dashboards.admin.\
@@ -28,7 +30,7 @@ from openstack_dashboard.dashboards.admin.\
 from openstack_dashboard.dashboards.admin.\
     shares.tables import SecurityServiceTable
 from openstack_dashboard.dashboards.admin.\
-    shares.tables import ShareNetworkTable
+    shares import tables as share_net_tables
 from openstack_dashboard.dashboards.admin.\
     shares.tables import ShareServerTable
 from openstack_dashboard.dashboards.admin.\
@@ -139,24 +141,39 @@ class SecurityServiceTab(tabs.TableTab):
 
 
 class ShareNetworkTab(tabs.TableTab):
-    table_classes = (ShareNetworkTable,)
     name = _("Share Networks")
     slug = "share_networks_tab"
     template_name = "horizon/common/_detail_table.html"
+
+    def __init__(self, tab_group, request):
+        if base.is_service_enabled(request, 'network'):
+            self.table_classes = (share_net_tables.NeutronShareNetworkTable,)
+        else:
+            self.table_classes = (share_net_tables.NovaShareNetworkTable,)
+        super(ShareNetworkTab, self).__init__(tab_group, request)
 
     def get_share_networks_data(self):
         try:
             share_networks = manila.share_network_list(
                 self.request, detailed=True, search_opts={'all_tenants': True})
-            neutron_net_names = dict([(net.id, net.name) for net in
-                                      neutron.network_list(self.request)])
-            neutron_subnet_names = dict([(net.id, net.name) for net in
-                                      neutron.subnet_list(self.request)])
-            for share in share_networks:
-                share.neutron_net = neutron_net_names.get(
-                    share.neutron_net_id) or share.neutron_net_id
-                share.neutron_subnet = neutron_subnet_names.get(
-                    share.neutron_subnet_id) or share.neutron_net_id
+            if base.is_service_enabled(self.request, 'network'):
+                neutron_net_names = dict([(net.id, net.name) for net in
+                                          neutron.network_list(self.request)])
+                neutron_subnet_names = dict([(net.id, net.name) for net in
+                                          neutron.subnet_list(self.request)])
+                for sn in share_networks:
+                    sn.neutron_net = neutron_net_names.get(
+                        sn.neutron_net_id) or sn.neutron_net_id or "-"
+                    sn.neutron_subnet = neutron_subnet_names.get(
+                        sn.neutron_subnet_id) or sn.neutron_subnet_id or "-"
+            else:
+                nova_net_names = dict(
+                    [(net.id, net.label)
+                     for net in network.network_list(self.request)])
+                for sn in share_networks:
+                    sn.nova_net = nova_net_names.get(
+                        sn.nova_net_id) or sn.nova_net_id or "-"
+
         except Exception:
             share_networks = []
             exceptions.handle(self.request,
